e0a09a319c344a7af376063c7f8a432ffe4f7028
Fernando Neuberger <fernando.neuberger@cmsw.com>
2023-05-07 16:30:27 -0300

    feat: Atualização da estrutura do servidor e adicionado mysql ao docker-compose

--- node/src/server.ts
+++ node/src/server.ts
@@ -1,22 +1,5 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
-import usersRouter from './routes/users';
-
-dotenv.config();
-
-const app = express();
-
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-//ROUTES
-app.use('/api/users', usersRouter)
+import { app } from "./app";
 
 const port = process.env.PORT
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+app.listen(port, () => console.log(`Server running on port ${port}`));
